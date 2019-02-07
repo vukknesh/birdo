7025db25168fa19aa08565913fcf684916d53e22
Leonardo Monte Castro Neshich <46389448+vukknesh@users.noreply.github.com>
2019-02-07 16:20:40 -0200

    Add files via upload

---
+++ util/Yelp.js
@@ -0,0 +1,35 @@
+const apiKey = "3j0ZTqRUqP8hrTGi14Ye_tpAyvM4lDh0jPWpnb7diGoZE28ob1doj5MabLlH3Sj7CIv_BAkGUAlbTisS5priRxiXd3OTxAY10cle0-v2Qiewwu-H5WbOGqzEFG1PXHYx"
+
+const Yelp = {
+
+	searchYelp(term, location, sortBy) {
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+			headers: {
+				Authorization: `Bearer ${apiKey}`,
+
+			},
+		}).then((response) => {
+			return response.json();
+		}).then((jsonResponse) => {
+			if (jsonResponse.businesses) {
+				return jsonResponse.businesses.map(((business) => {
+					console.log(business);
+					return {
+						id: business.id,
+						imageSrc: business.image_url,
+						name: business.name,
+						address: business.location.address1,
+						city: business.location.city,
+						state: business.location.state,
+						zipCode: business.location.zip_code,
+						category: business.categories[0].title,
+						rating: business.rating,
+						reviewCount: business.review_count,
+					};
+				}));
+			}
+		})
+	}
+};
+
+export default Yelp;
